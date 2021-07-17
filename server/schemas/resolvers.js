232f59d19e2b0224c903517f1a20f38e39f3fd7e
lucile <lucile.thou@mybeeye.com>
2021-07-16 22:46:34 -0400

    added auth type and me query + mutation to addUser and to login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,47 @@
-const { User, Book } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    Query:{
-        books : async () => {
-            return Book.find()
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('books')
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
+
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('books');
+        }
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            return user;
+        },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('That user does not exist');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
         }
     }
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,8 +10,26 @@ const typeDefs = gql`
     title: String!
   }
 
-  type Query {
+  type User {
+    _id: ID
+    username: String!
+    email: String!
     books: [Book]
+  }
+
+  type Query {
+    me: User
+    users: [User]
+  }
+
+  type Auth{
+    token: ID!
+    user: User
+  }
+
+  type Mutation{
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): User
   }
 `;
 
